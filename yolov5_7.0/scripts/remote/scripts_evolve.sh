04be2334b318eb367510a71b32ebfb03962b12d0
MarToonLi <1437623218@qq.com>
2025-10-30 22:59:20 +0800

    feat: consider evolve

--- yolov5_7.0/scripts/remote/scripts_evolve.sh
+++ yolov5_7.0/scripts/remote/scripts_evolve.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ================ remote ====================
 # =============== Single-GPU ================
-python ../../train.py       --device 0 --batch 24 --imgsz 1120 --epochs 10  --evolve
+nohup python ../../train_evolve.py  --device 0 --batch 24 --imgsz 1120 --epochs 10 --cache  --evolve  --cos-lr  --image-weights > output_evolve.log 2>&1 &
 #nohup python ../../train.py --device 0 --batch 24 --imgsz 1120 --epochs 10  --evolve > output_evolve.log 2>&1 &
 # 远程时似乎不适合使用cache，会占用大量内存，导致训练失败
 
