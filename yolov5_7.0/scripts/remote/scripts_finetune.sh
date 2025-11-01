4d6f0bb533b13616fea169cb647f098f62dfdafe
Li Xutong <1437623218@qq.com>
2025-11-02 00:50:49 +0800

    feat: update raytune

--- yolov5_7.0/scripts/remote/scripts_finetune.sh
+++ yolov5_7.0/scripts/remote/scripts_finetune.sh
@@ -5,7 +5,8 @@
 # python train.py  --batch-size 64 --data coco.yaml --weights yolov5s.pt --device 0
 # python ../../train.py       --device 0 --batch 24 --cache --imgsz 1120 
 # nohup python ../../train_sgdm.py --device 0 --batch 24 --epochs 200 --cache --imgsz 1120 --cos-lr --image-weights --optimizer SGD  > output_train.log 2>&1 &
-nohup python ../../train_little.py --device 0 --batch 24 --epochs 80 --cache --imgsz 1120 --cos-lr  --image-weights  --freeze 10  > output_finetune.log 2>&1 &
+# nohup python ../../train_little.py --device 0 --batch 24 --epochs 80 --cache --imgsz 1120 --cos-lr  --image-weights  --freeze 10  > output_finetune.log 2>&1 &
+nohup python ../../train_raytune.py --device 0 --batch 24 --epochs 500 --cache --imgsz 1120 --cos-lr  --image-weights  --freeze 10  > output_raytune.log 2>&1 &
 
 # ============== Multi-GPU with delay ================
 # nohup python -m torch.distributed.run --nproc_per_node 2 train.py --device 0,1 --batch 96  > output_2.log 2>&1 &
